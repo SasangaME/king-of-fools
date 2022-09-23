bc9d5d83dddf8f126a6b1e098bc75deafb5d332e
SasangaME <madhumaledirisinghe@gmail.com>
2022-09-23 14:22:38 +0530

    unit testing ETH deposits

--- web3/test/king-of-fools-test.js
+++ web3/test/king-of-fools-test.js
@@ -1,19 +1,42 @@
 const { ethers, hre } = require("hardhat");
+const { expect } = require("chai");
 
 describe("King of the Fools", function () {
-    it("test", async function () {
-        const network = await ethers.getDefaultProvider().getNetwork();
-        const [owner, user1, user2] = await ethers.getSigners();
 
-        // dpeloy fool contract
+    async function getContract() {
+        const MockUSDC = await ethers.getContractFactory("MockUSDC");
+        const usdc = await MockUSDC.deploy();
         const KingOfTheFools = await ethers.getContractFactory("KingOfTheFools");
-        const kof = await KingOfTheFools.deploy();
-        const tx1 = await kof.connect(user1).depositEth({ value: ethers.utils.parseEther("1") });
-        await tx1.wait();
-        const tx2 = await kof.connect(user2).depositEth({ value: ethers.utils.parseEther("1.5") });
-        await tx2.wait();
+        const kof = await KingOfTheFools.deploy(usdc.address);
+        return kof;
+    }
 
-        const user1Balance = await kof.balances(user1.address);
-        console.log("user1 balance: " + ethers.utils.formatEther(user1Balance.ethAmount));
+    it("eth not fool", async function () {
+        // const network = await ethers.getDefaultProvider().getNetwork();
+        const [user1, user2] = await ethers.getSigners();
+
+        // dpeloy contracts
+        const kof = await getContract();
+
+        await kof.connect(user1).depositEth({ value: ethers.utils.parseEther("1") });
+        const tx2 = await kof.connect(user2).depositEth({ value: ethers.utils.parseEther("1.6") });
+        const reciept2 = await tx2.wait();
+        const event2 = await reciept2.events[0].args;
+        const isFool = event2.isFool;
+        expect(isFool).to.equal(false);
+    });
+
+    it("eth fool", async function () {
+        const [user1, user2] = await ethers.getSigners();
+
+        // dpeloy contracts
+        const kof = await getContract();
+
+        await kof.connect(user1).depositEth({ value: ethers.utils.parseEther("1") });
+        const tx2 = await kof.connect(user2).depositEth({ value: ethers.utils.parseEther("1") });
+        const reciept2 = await tx2.wait();
+        const event2 = await reciept2.events[0].args;
+        const isFool = event2.isFool;
+        expect(isFool).to.equal(true);
     });
 });
